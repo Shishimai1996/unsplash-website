9be2620195f90da783782654adbc571a0c889436
Mai Shimizu/舞 清水 <159525515+Shishimai1996@users.noreply.github.com>
2025-04-15 11:33:09 +0900

    check mjs

--- unsplash-fe/next.config.mjs
+++ unsplash-fe/next.config.mjs
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.DEPLOY_TARGET === 'GH_PAGES'
 const nextConfig = {
     images: {
       domains: ['images.unsplash.com', 'plus.unsplash.com'],
     },
-    output: 'export'
-  };
-  
-  const isGithubPages = process.env.DEPLOY_TARGET === 'GH_PAGES'
-
-  module.exports = {
+    output: 'export',
     basePath: isGithubPages ? '/unsplash-website' : '',
     assetPrefix: isGithubPages? '/unsplash-website': '',
     trailingSlash: true
-  }
+  };
+
 export default nextConfig
