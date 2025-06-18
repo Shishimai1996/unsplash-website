c66695db836ff46cf4e224c94e2ea0693f1a97a8
Mai Shimizu/舞 清水 <159525515+Shishimai1996@users.noreply.github.com>
2025-06-18 16:33:08 +0900

    setup render for bf deploy

--- unsplash-fe/next.config.mjs
+++ unsplash-fe/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     images: {
       domains: ['images.unsplash.com', 'plus.unsplash.com'],
     },
-    output: 'export',
+    // output: 'export',
     basePath: isGithubPages ? '/unsplash-website' : '',
     assetPrefix: isGithubPages? '/unsplash-website/': '',
     trailingSlash: true
